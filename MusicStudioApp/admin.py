cba9a9da9ef6a7f2597090ef193c3c6b93558b29
GrimmOT <ashutov2009@gmail.com>
2023-02-19 12:14:39 +0300

    DB

--- MusicStudioApp/admin.py
+++ MusicStudioApp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import *
+
+admin.site.register(Group)
+
+admin.site.register(Grade)
+
+admin.site.register(Song)
+
+admin.site.register(Programm)
+
+admin.site.register(Instruments)
--- MusicStudioApp/models.py
+++ MusicStudioApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length = 150)
+
+
+class Grade(models.Model):
+    name = models.CharField(max_length = 150)
+
+
+class Programm(models.Model):
+    text = models.CharField(max_length = 150)
+
+
+class Song(models.Model):
+    name = models.CharField(max_length = 150)
+
+
+class Instruments(models.Model):
+    name = models.CharField(max_length = 150)
