b1b3de1847c0a2350bfce9069686dfadb9943d2d
GrimmOT <ashutov2009@gmail.com>
2023-04-09 12:58:25 +0300

    MusicStudio

--- MusicStudioApp/static/js/gallery.js
+++ MusicStudioApp/static/js/gallery.js
@@ -1,12 +1,21 @@
 let popup = document.querySelector(".popup-background")
 let cross = document.querySelector(".close")
-let photo = document.querySelector(".photo")
-function openPopup(){
+let photos = document.querySelectorAll(".concert-photo")
+let body = document.querySelector("body")
+let popupImage = popup.querySelector(".popup-image img")
+
+for(let i = 0; i < photos.length; i++){
+    photos[i].addEventListener("click", openPopup)
+}
+
+function openPopup(event){
     popup.style.display = "flex"
+    body.classList.add("disable-scroll")
+    let imageUrl = event.target.getAttribute("src")
+    popupImage.setAttribute("src", imageUrl)
 }
 function closePopup(){
     popup.style.display = "none"
+    body.classList.remove("disable-scroll")
 }
-photo.addEventListener("click", openPopup)
 cross.addEventListener("click", closePopup)
-popup.addEventListener("click", closePopup)
