5551bce4845ee55b6cf588536819354c268cf944
GrimmOT <ashutov2009@gmail.com>
2023-05-10 18:29:46 +0300

    MusicStudio

--- MusicStudioApp/static/js/concert.js
+++ MusicStudioApp/static/js/concert.js
@@ -6,17 +6,19 @@ const swiper = new Swiper('.swiper', {
     pagination: {
       el: '.swiper-pagination',
     },
-    slidesPerView: 2,
+    slidesPerView: 1,
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
+    breakpoints: {
+      820: {
+        slidesPerView: 2
+      }
+    }
     // // And if we need scrollbar
     // scrollbar: {
     //   el: '.swiper-scrollbar',
     // },
   });
-
-  swiper.slidesPerView
--- MusicStudioApp/static/js/group.js
+++ MusicStudioApp/static/js/group.js
@@ -1,5 +1,14 @@
 let backButton = document.querySelector("#back-button")
+let member = document.querySelectorAll(".member")
+for (let i = 0; i < member.length; i++){
+    member[i].addEventListener("click", memberHover)
+}
+
+function memberHover(event){
+    event.target.classList.toggle("hover-on")
+}
 
 backButton.addEventListener("click", (event) => {
     window.history.back()
 })
+
